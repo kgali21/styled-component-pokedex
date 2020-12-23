41a242cad12bf58bfde959d6e6f9f2e6f64d375a
Kamran <kgali21@gmail.com>
2020-12-23 12:09:38 -0800

    pokedex api no longer works find new api

--- src/services/pokeFetch.js
+++ src/services/pokeFetch.js
@@ -7,3 +7,5 @@ export const onePokeFetch = (search, page, perPage) => {
   return fetch(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${search}&page=${page}&perPage=${perPage}`)
     .then(res => res.json());
 };
+
+//Api no longer works, find new api and fix data
